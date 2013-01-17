6b858e273a9a96dea05d92c5ec8975d9bc6c944e
Patrick Michaud <pmichaud@uw.edu>
2013-01-16 16:18:34 -0800

    A start of the userservice, with override app.  Denies for now - needs to include the authz_group project

---
+++ userservice/user.py
@@ -0,0 +1,96 @@
+from django.http import HttpRequest
+from django.conf import settings
+import logging
+
+from threading import currentThread
+
+class UserService:
+    _user_data = {}
+    logger = logging.getLogger(__name__)
+
+    def _get_current_user_data(self):
+        return UserService._user_data[currentThread()]
+
+    def _require_middleware(self):
+        if not "initialized" in self._get_current_user_data():
+            print "You need to have this line in your MIDDLEWARE_CLASSES:"
+            print "'userservice.user.UserServiceMiddleware',"
+
+            raise Exception("You need the UserServiceMiddleware")
+
+    def get_user(self):
+        self._require_middleware()
+
+        override = self.get_override_user()
+        if override and len(override) > 0:
+            return override
+
+        actual = self.get_original_user()
+        if not actual or len(actual) == 0:
+            return self._get_authenticated_user()
+        return actual
+
+    def get_original_user(self):
+        user_data = self._get_current_user_data()
+        if "original_user" in user_data:
+            return user_data["original_user"]
+
+    def get_override_user(self):
+        user_data = self._get_current_user_data()
+        if "override_user" in user_data:
+            return user_data["override_user"]
+
+    def set_user(self, user):
+        user_data = self._get_current_user_data()
+        user_data["original_user"] = user
+        user_data["session"]["_us_user"] = user
+
+    def set_override_user(self, override):
+        user_data = self._get_current_user_data()
+        user_data["override_user"] = override
+        user_data["session"]["_us_override"] = override
+
+    def clear_override(self):
+        user_data = self._get_current_user_data()
+        user_data["override_user"] = None
+        user_data["session"]["_us_override"] = None
+
+
+class UserServiceMiddleware(object):
+
+    logger = logging.getLogger(__name__)
+
+    def process_request(self, request):
+        thread = currentThread()
+        UserService._user_data[thread] = {}
+        UserService._user_data[thread]["initialized"] = True
+
+        session = request.session
+        UserService._user_data[thread]["session"] = session
+
+        if not "_us_user" in session:
+            user = self._get_authenticated_user(request)
+            if user:
+                UserService._user_data[thread]["original_user"] = user
+                UserService._user_data[thread]["session"]["_us_user"] = user
+        else:
+            UserService._user_data[thread]["original_user"] = session["_us_user"]
+
+        if "_us_override" in session:
+            UserService._user_data[thread]["override_user"] = session["_us_override"]
+
+    def process_response(self, request, response):
+        thread = currentThread()
+        UserService._user_data[thread] = {}
+        return response
+
+    def _get_authenticated_user(self, request):
+        netid = None
+        if settings.DEBUG:
+            netid = 'javerage'
+        else:
+            netid = request.user.username
+
+        return netid
+
+
---
+++ userservice/views.py
@@ -0,0 +1,48 @@
+from django.http import HttpResponseRedirect, HttpResponse
+from django.shortcuts import render_to_response
+from django.template import RequestContext
+from django.conf import settings
+from userservice.user import UserService
+import logging
+
+def support(request):
+    #timer = Timer()
+    logger = logging.getLogger(__name__)
+
+    user_service = UserService()
+    user_service.get_user()
+    # Do the group auth here.
+
+    if not hasattr(settings, "USERSERVICE_ADMIN_GROUP"):
+        print "You must have a group defined as your admin group."
+        print 'Configure that using USERSERVICE_ADMIN_GROUP="foo_group"'
+        raise Exception("Missing USERSERVICE_ADMIN_GROUP in settings")
+
+    actual_user = user_service.get_original_user()
+    if not actual_user:
+#        log_invalid_netid_response(logger, timer)
+        return invalid_session()
+
+#    gws = GWS()
+#    is_admin = gws.is_effective_member(settings.MYUW_ADMIN_GROUP, actual_user)
+
+    is_admin = True
+    if is_admin == False:
+        return  HttpResponseRedirect("no_access.html")
+
+    if "override_as" in request.POST:
+        user_service.set_override_user(request.POST["override_as"].strip())
+
+    if "clear_override" in request.POST:
+        user_service.clear_override()
+
+    context = {
+        'original_user': user_service.get_original_user(),
+        'override_user': user_service.get_override_user(),
+    }
+#    log_success_response(logger, timer)
+    return render_to_response('support.html',
+                              context,
+                              context_instance=RequestContext(request))
+
+
