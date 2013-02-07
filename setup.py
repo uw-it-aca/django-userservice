ed3c1f52698b9009d01b7df6885dbde7b6f59e78
Patrick Michaud <pmichaud@uw.edu>
2013-02-07 14:05:42 -0800

    Adding a setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='Django-UserService',
+      version='1.0',
+      description='Django user impersonation',
+      install_requires=['Django==1.4'],
+     )
