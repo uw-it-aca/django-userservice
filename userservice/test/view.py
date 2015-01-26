93709852116b700b7e1cbfe849c1862d06e9117a
Patrick Michaud <pmichaud@uw.edu>
2015-01-26 08:47:11 -0800

    view test updates.
    moving the test to the new api.  Also adding tests around custom user validation

--- userservice/test/view.py
+++ userservice/test/view.py
@@ -5,8 +5,9 @@ from django.test.client import RequestFactory
 from django.test.utils import override_settings
 from unittest2 import skipIf
 from django.core.urlresolvers import reverse
-from userservice.test import get_user
-from userservice.user import UserServiceMiddleware, UserService
+from userservice.test import get_user as get_django_user
+from userservice.user import get_user, get_acting_user, get_override_user
+from userservice.user import get_original_user, set_override_user
 
 def missing_url(name):
     try:
@@ -36,36 +37,81 @@ class TestView(TestCase):
     def test_override(self):
         c = Client()
 
-        get_user('javerage')
+        get_django_user('javerage')
         c.login(username='javerage', password='pass')
-
-        response = c.post(reverse("override"), { "override_as": "testover" })
 
         request = RequestFactory().get("/")
         request.session = c.session
-        request.user = get_user('javerage')
+        request.user = get_django_user('javerage')
 
-        UserServiceMiddleware().process_request(request)
+        response = c.post(reverse("override"), { "override_as": "testover" })
+        request.session = c.session
 
-        self.assertEquals(UserService().get_user(), 'testover')
-        self.assertEquals(UserService().get_acting_user(), 'javerage')
-        self.assertEquals(UserService().get_original_user(), 'javerage')
-        self.assertEquals(UserService().get_override_user(), 'testover')
-
-        UserServiceMiddleware().process_response(request, response)
+        self.assertEquals(get_user(request), 'testover')
+        self.assertEquals(get_acting_user(request), 'javerage')
+        self.assertEquals(get_original_user(request), 'javerage')
+        self.assertEquals(get_override_user(request), 'testover')
 
         response = c.post(reverse("override"), { "clear_override": 1})
 
         request = RequestFactory().get("/")
         request.session = c.session
-        request.user = get_user('javerage')
 
-        UserServiceMiddleware().process_request(request)
+        self.assertEquals(get_user(request), 'javerage')
+        self.assertEquals(get_acting_user(request), 'javerage')
+        self.assertEquals(get_original_user(request), 'javerage')
+        self.assertEquals(get_override_user(request), None)
 
-        self.assertEquals(UserService().get_user(), 'javerage')
-        self.assertEquals(UserService().get_acting_user(), 'javerage')
-        self.assertEquals(UserService().get_original_user(), 'javerage')
-        self.assertEquals(UserService().get_override_user(), None)
+    @override_settings(AUTHZ_GROUP_BACKEND = 'authz_group.authz_implementation.all_ok.AllOK',
+                       USERSERVICE_VALIDATION_MODULE='userservice.test.view.under8')
+    def test_validation(self):
+        c = Client()
 
-        UserServiceMiddleware().process_response(request, response)
+        get_django_user('javerage')
+        c.login(username='javerage', password='pass')
 
+
+        request = RequestFactory().get("/")
+        request.session = c.session
+        request.user = get_django_user('javerage')
+
+        response = c.post(reverse("override"), { "override_as": "testover8" })
+
+        self.assertEquals(get_user(request), 'javerage')
+        self.assertEquals(get_acting_user(request), 'javerage')
+        self.assertEquals(get_original_user(request), 'javerage')
+        self.assertEquals(get_override_user(request), None)
+
+    @override_settings(AUTHZ_GROUP_BACKEND = 'authz_group.authz_implementation.all_ok.AllOK',
+                       USERSERVICE_VALIDATION_MODULE='userservice.test.view.over8')
+    def test_validation(self):
+        c = Client()
+
+        get_django_user('javerage')
+        c.login(username='javerage', password='pass')
+
+
+        request = RequestFactory().get("/")
+        request.session = c.session
+        request.user = get_django_user('javerage')
+
+        response = c.post(reverse("override"), { "override_as": "testover8" })
+
+        self.assertEquals(get_user(request), 'testover8')
+        self.assertEquals(get_acting_user(request), 'javerage')
+        self.assertEquals(get_original_user(request), 'javerage')
+        self.assertEquals(get_override_user(request), 'testover8')
+
+
+
+
+def under8(username):
+    if len(username) < 8:
+        return None
+    return "Bad len"
+
+
+def over8(username):
+    if len(username) > 8:
+        return None
+    return "Bad len"
