12aa73bf9fd2fd10c9deba35bbc7044ab8d5b6ad
Patrick Michaud <pmichaud@uw.edu>
2016-10-28 12:07:20 -0700

    Adding template config for django 1.10

--- travis-ci/settings.py
+++ travis-ci/settings.py
@@ -45,6 +45,22 @@ if django_v in ("1.6.*", "1.7.*", "1.8.*", "1.9.*"):
     new_style = False
 
 if new_style:
+    TEMPLATES = [
+        {
+            'BACKEND': 'django.template.backends.django.DjangoTemplates',
+            'DIRS': [],
+            'APP_DIRS': True,
+            'OPTIONS': {
+                'context_processors': [
+                    'django.template.context_processors.debug',
+                    'django.template.context_processors.request',
+                    'django.contrib.auth.context_processors.auth',
+                    'django.contrib.messages.context_processors.messages',
+                ],
+            },
+        },
+    ]
+
     MIDDLEWARE = [
         'django.middleware.security.SecurityMiddleware',
         'django.contrib.sessions.middleware.SessionMiddleware',
