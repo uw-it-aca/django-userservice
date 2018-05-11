057b1dfcb00c78a4de930fca5fec79185eb9a8d0
Jim Laney <jlaney@uw.edu>
2018-05-11 08:46:59 -0700

    fix paths for test modules

--- userservice/test/__init__.py
+++ userservice/test/__init__.py
@@ -8,3 +8,23 @@ def get_user(username):
     except Exception as ex:
         user = User.objects.create_user(username, password='pass')
         return user
+
+
+def can_override(request):
+    return True
+
+
+def add_washington(username):
+    return "%s@uw.edu" % username
+
+
+def under8(username):
+    if len(username) < 8:
+        return None
+    return "Bad len"
+
+
+def over8(username):
+    if len(username) > 8:
+        return None
+    return "Bad len"
