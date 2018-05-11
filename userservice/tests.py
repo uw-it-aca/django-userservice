1f2c134ef41529e895dbbf1669fda30a4204882f
Jim Laney <jlaney@uw.edu>
2018-05-11 08:31:13 -0700

    renames test files and drops tests.py

--- userservice/tests.py
+++
@@ -1,4 +0,0 @@
-from userservice.test.middleware import TestMiddleware
-from userservice.test.api import TestAPI
-from userservice.test.view import TestView
-from userservice.test.test_username import TestUserName
