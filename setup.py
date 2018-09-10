6ce9716bcaf39cf46e94e7b60fbd51fb9c4662d5
Fang Lin <fanglin@uw.edu>
2018-09-10 16:33:30 -0700

    django version

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
     author_email="aca-it@uw.edu",
     include_package_data=True,
     install_requires=[
-        'django>=2.1,<3.0',
+        'django>2.1,<3.0',
         'unittest2',
     ],
     license='Apache License, Version 2.0',
